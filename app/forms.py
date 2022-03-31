08decece75532954610ba4243ef5b59235d464af
Brandon Domangue <12216617+lowtied@users.noreply.github.com>
2022-03-31 10:06:53 -0500

    forms file fixup

--- app/forms.py
+++ app/forms.py
@@ -4,22 +4,20 @@ from wtforms import (StringField,
                      BooleanField,
                      SubmitField,
                      DateField,
-                     FloatField)
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
-    Length, NumberRange
+                     FloatField,
+                     EmailField)
+from wtforms.validators import (DataRequired,
+                                Email,
+                                EqualTo,
+                                Length,
+                                NumberRange,
+                                Optional)
 from .models.user import User
-
-
-def validate_username(self, username) -> None:
-    user = User.query.filter_by(username=self.username.data).first()
-    if user is not None:
-        raise ValidationError('Username already taken.')
-
-
-def validate_email(self, email) -> None:
-    user = User.query.filter_by(email=self.email.data).first()
-    if user is not None:
-        raise ValidationError('Email address already taken.')
+from .utils.form_validators import (validate_email,
+                                    validate_username,
+                                    validate_password,
+                                    validate_new_password,
+                                    validate_new_username, validate_new_email)
 
 
 class LoginForm(FlaskForm):
@@ -31,15 +29,25 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField(
-        'Username', validators=[DataRequired(), validate_username]
+        'Username',
+        validators=[DataRequired(), validate_username]
     )
+
     email = StringField(
-        'Email', validators=[DataRequired(), Email(), validate_email]
+        'Email',
+        validators=[DataRequired(), Email(), validate_email]
     )
-    password = PasswordField('Password', validators=[DataRequired()])
+
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=4)]
+    )
+
     password_repeat = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo('password')]
     )
+
     submit = SubmitField('Register')
 
 
